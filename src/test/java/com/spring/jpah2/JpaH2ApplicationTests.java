f63cc4dd5555c65688a070034897272f8192e165
leoee <leonardoe1997@gmail.com>
2021-12-18 19:28:00 -0300

    Add initial base project

---
+++ src/test/java/com/spring/jpah2/JpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
